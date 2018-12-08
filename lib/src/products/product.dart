a4b8b3a96d021db1f2613dc13c4f78df4887003d
jinxin479 <jinxin479@126.com>
2018-12-08 23:55:24 +0800

    learn lifecycle

---
+++ lib/src/products/product.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Products extends StatelessWidget {
+  final List<String> products;
+  Products([this.products = const []]) {
+    print('[Products Widget] Constructor');
+  }
+  @override
+  Widget build(BuildContext context) {
+    // print(products);
+    return Column(
+        children: products
+            .map((element) => Card(
+                  child: Column(
+                    children: <Widget>[
+                      Image.asset("assets/images/2.jpg", fit: BoxFit.cover),
+                      Text(element)
+                    ],
+                  ),
+                ))
+            .toList());
+  }
+}
---
+++ lib/src/products/productsManager.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+import './product.dart';
+
+class ProductsManager extends StatefulWidget {
+  final String initialProduct;
+  ProductsManager({this.initialProduct}) {
+    print('[ProductsManager Widget] Constructor');
+  }
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    print('[ProductsManager Widget] createState');
+    return new ProductsManagerState();
+  }
+}
+
+class ProductsManagerState extends State<ProductsManager> {
+  List<String> _products = [];
+  @override
+  void initState() {
+    _products.add(widget.initialProduct);
+    super.initState();
+  }
+
+  @override
+  void didUpdateWidget(ProductsManager oldWidget) {
+    print(1);
+    print(widget.initialProduct);
+    super.didUpdateWidget(oldWidget);
+    // setState(() {
+    //      _products.map((index))
+    //     });
+  }
+
+  _addCard() {
+    print("222");
+    setState(() {
+      _products.add(WordPair.random().asCamelCase);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Column(children: <Widget>[
+      Container(
+        margin: EdgeInsets.all(10),
+        child: RaisedButton(
+          child: Text('Add Card'),
+          color: Theme.of(context).primaryColor,
+          onPressed: _addCard,
+        ),
+        alignment: Alignment.center,
+      ),
+      new Products(_products)
+    ]);
+  }
+}
