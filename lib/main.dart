541ce32a8b7eb136516b40d3d7e731ed6ff74b56
金 鑫 <jinxin@hualala.com>
2019-03-27 20:15:45 +0800

    动画

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:random_pk/random_pk.dart';
+import 'package:flutter_app/src/animatedDemo.dart';
 
 void main ()=>runApp(App());
 class App extends StatelessWidget {
@@ -7,68 +7,9 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Title',
-      home: SizeBoxDemo(),
+      home: AnimatedDemo(),
     );
   }
 }
 
-class SizeBoxDemo extends StatelessWidget{
-  @override
-  Widget build(BuildContext context) {
-	  var deviceSize = MediaQuery.of(context).size;
-	  print(deviceSize);
-      return MaterialApp(
-        title: "Demo Page",
-        theme: ThemeData(primarySwatch: Colors.blue),
-        home:Scaffold(
-			appBar: new AppBar(
-					title: const Text('Widget Demo'),
-				),
-			body: SafeArea(
-				child:Center(
-					child: Column(
-					children: <Widget>[
-						SizedBox(
-							width: 100.0,
-             				height: 100.0,
-							child: RandomContainer(
-								width: 50,
-								height: 50,
-							),
-							),
-						SizedBox(
-						height: 20,
-						),
-						RandomContainer(
-							child: SizedBox.fromSize(
-								size: deviceSize / 10.0,
-							),
-						),
-						RandomContainer(
-							height: 200.0,
-							width: 200.0,
-							child: SizedBox.expand(
-								child: FlutterLogo(
-									size:100
-								),
-							),
-						),
-						RandomContainer(
-							height: 100.0,
-							width: 100.0,
-							child: FractionallySizedBox(
-								heightFactor: 0.5,
-								widthFactor: 0.5,
-								child: FlutterLogo(),
-							),
-						)
-					],
-				),
-				)
-				
-			),
-		)
-      );
-    }
-}
 
