150e1d815709e460ecdd2be919d6fa40a5d31f52
金 鑫 <jinxin@hualala.com>
2019-04-06 15:25:53 +0800

    d

--- lib/src/models/chat_model.dart
+++ lib/src/models/chat_model.dart
@@ -44,4 +44,109 @@ List<ChatModel> dummyData = [
       time: "15:30",
       avatarUrl:
           "http://www.usanetwork.com/sites/usanetwork/files/styles/629x720/public/suits_cast_harvey.jpg?itok=fpTOeeBb"),
+            new ChatModel(
+      name: "Barry Allen",
+      message: "I'm the fastest man alive!",
+      time: "12:30",
+      avatarUrl:
+          "http://www.usanetwork.com/sites/usanetwork/files/styles/629x720/public/suits_cast_harvey.jpg?itok=fpTOeeBb"),
+  new ChatModel(
+      name: "Joe West",
+      message: "Hey Flutter, You are so cool !",
+      time: "15:30",
+      avatarUrl:
+          "http://www.usanetwork.com/sites/usanetwork/files/styles/629x720/public/suits_cast_harvey.jpg?itok=fpTOeeBb"),
+
+  new ChatModel(
+      name: "Barry Allen",
+      message: "I'm the fastest man alive!",
+      time: "12:30",
+      avatarUrl:
+          "http://www.usanetwork.com/sites/usanetwork/files/styles/629x720/public/suits_cast_harvey.jpg?itok=fpTOeeBb"),
+  new ChatModel(
+      name: "Joe West",
+      message: "Hey Flutter, You are so cool !",
+      time: "15:30",
+      avatarUrl:
+          "http://www.usanetwork.com/sites/usanetwork/files/styles/629x720/public/suits_cast_harvey.jpg?itok=fpTOeeBb"),
+
+  new ChatModel(
+      name: "Barry Allen",
+      message: "I'm the fastest man alive!",
+      time: "12:30",
+      avatarUrl:
+          "http://www.usanetwork.com/sites/usanetwork/files/styles/629x720/public/suits_cast_harvey.jpg?itok=fpTOeeBb"),
+  new ChatModel(
+      name: "Joe West",
+      message: "Hey Flutter, You are so cool !",
+      time: "15:30",
+      avatarUrl:
+          "http://www.usanetwork.com/sites/usanetwork/files/styles/629x720/public/suits_cast_harvey.jpg?itok=fpTOeeBb"),
+
+  new ChatModel(
+      name: "Barry Allen",
+      message: "I'm the fastest man alive!",
+      time: "12:30",
+      avatarUrl:
+          "http://www.usanetwork.com/sites/usanetwork/files/styles/629x720/public/suits_cast_harvey.jpg?itok=fpTOeeBb"),
+  new ChatModel(
+      name: "Joe West",
+      message: "Hey Flutter, You are so cool !",
+      time: "15:30",
+      avatarUrl:
+          "http://www.usanetwork.com/sites/usanetwork/files/styles/629x720/public/suits_cast_harvey.jpg?itok=fpTOeeBb"),
+
+  new ChatModel(
+      name: "Barry Allen",
+      message: "I'm the fastest man alive!",
+      time: "12:30",
+      avatarUrl:
+          "http://www.usanetwork.com/sites/usanetwork/files/styles/629x720/public/suits_cast_harvey.jpg?itok=fpTOeeBb"),
+  new ChatModel(
+      name: "Joe West",
+      message: "Hey Flutter, You are so cool !",
+      time: "15:30",
+      avatarUrl:
+          "http://www.usanetwork.com/sites/usanetwork/files/styles/629x720/public/suits_cast_harvey.jpg?itok=fpTOeeBb"),
+
+  new ChatModel(
+      name: "Barry Allen",
+      message: "I'm the fastest man alive!",
+      time: "12:30",
+      avatarUrl:
+          "http://www.usanetwork.com/sites/usanetwork/files/styles/629x720/public/suits_cast_harvey.jpg?itok=fpTOeeBb"),
+  new ChatModel(
+      name: "Joe West",
+      message: "Hey Flutter, You are so cool !",
+      time: "15:30",
+      avatarUrl:
+          "http://www.usanetwork.com/sites/usanetwork/files/styles/629x720/public/suits_cast_harvey.jpg?itok=fpTOeeBb"),
+
+  new ChatModel(
+      name: "Barry Allen",
+      message: "I'm the fastest man alive!",
+      time: "12:30",
+      avatarUrl:
+          "http://www.usanetwork.com/sites/usanetwork/files/styles/629x720/public/suits_cast_harvey.jpg?itok=fpTOeeBb"),
+  new ChatModel(
+      name: "Joe West",
+      message: "Hey Flutter, You are so cool !",
+      time: "15:30",
+      avatarUrl:
+          "http://www.usanetwork.com/sites/usanetwork/files/styles/629x720/public/suits_cast_harvey.jpg?itok=fpTOeeBb"),
+
+  new ChatModel(
+      name: "Barry Allen",
+      message: "I'm the fastest man alive!",
+      time: "12:30",
+      avatarUrl:
+          "http://www.usanetwork.com/sites/usanetwork/files/styles/629x720/public/suits_cast_harvey.jpg?itok=fpTOeeBb"),
+  new ChatModel(
+      name: "Joe West",
+      message: "Hey Flutter, You are so cool !",
+      time: "15:30",
+      avatarUrl:
+          "http://www.usanetwork.com/sites/usanetwork/files/styles/629x720/public/suits_cast_harvey.jpg?itok=fpTOeeBb"),
+
+
 ];
