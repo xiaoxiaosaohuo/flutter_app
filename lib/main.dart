697762ba907df53c2474b05415e379fb69730adb
金 鑫 <jinxin@hualala.com>
2019-08-05 23:45:48 +0800

    q
    q

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/src/LoginPage.dart';
+import 'package:fluro/fluro.dart';
+import 'package:flutter_app/pages/LoginPage.dart';
+import 'package:flutter_app/routers/application.dart';
+import 'package:flutter_app/routers/routers.dart';
+
+const int ThemeColor = 0xFFC91B3A;
+
 void main ()=>runApp(MyApp());
 
-
 class MyApp extends StatelessWidget{
+
+  MyApp(){
+    final router = new Router();
+
+    Routes.configureRoutes(router);
+
+    Application.router = router;
+  }
+
   @override
   Widget build(BuildContext context) {
       return MaterialApp(
-        title: "Login Page",
-        theme: ThemeData(primarySwatch: Colors.blue),
-        home:LoginPage()
+        title: "Flutter",
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          primaryColor: Colors.blue,
+          ),
+        home:LoginPage(),
+        onGenerateRoute: Application.router.generator,
       );
     }
 }
---
+++ lib/routers/routers.dart
@@ -0,0 +1,18 @@
+import 'package:fluro/fluro.dart';
+import 'package:flutter/material.dart';
+import './router_handler.dart';
+
+class Routes{
+  static String root = "/";
+  static String home = "/home";
+  static String login = "/login";
+  static void configureRoutes(Router router) {
+    router.notFoundHandler = new Handler(
+        handlerFunc: (BuildContext context, Map<String, List<String>> params) {
+      print("ROUTE WAS NOT FOUND !!!");
+    });
+
+    router.define(login, handler: loginHandler);
+    router.define(home, handler: homeHandler);
+  }
+}
