d15233f75e4951bf5fa25787b4999b0caea15d4b
金 鑫 <jinxin@hualala.com>
2019-08-09 00:50:35 +0800

    计数器
    计数器

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,12 +10,22 @@ class HomePage extends StatefulWidget{
 }
 
 class HomePageState extends State<HomePage>{
+  int count = 0;
+  onPressed(){
+    this.setState((){
+      count++;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body:Center(
-          child: Text("HomePage"),
-        )
+          child: Text('$count'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: this.onPressed,
+          child: Icon(Icons.add),
+          ),
       );
   }
 }
