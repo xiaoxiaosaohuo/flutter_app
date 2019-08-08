a9eeaad356634581aec416112c61746665a5635e
金 鑫 <jinxin@hualala.com>
2019-08-09 01:13:14 +0800

    a
    a

---
+++ lib/widgets/animation/animate01.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+
+
+class Animate01 extends StatefulWidget {
+  _Animate01State createState() => _Animate01State();
+}
+
+class _Animate01State extends State<Animate01> with SingleTickerProviderStateMixin {
+  Animation<double> animation;
+  AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller =
+        AnimationController(duration: const Duration(seconds: 5), vsync: this);
+    // #docregion addListener
+    animation = Tween<double>(begin: 0, end: 300).animate(controller)
+      ..addListener(() {
+        // #enddocregion addListener
+        setState(() {
+          // The state that has changed here is the animation object’s value.
+        });
+        // #docregion addListener
+      });
+    // #enddocregion addListener
+    controller.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 10),
+        height: animation.value,
+        width: animation.value,
+        child: FlutterLogo(),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+}
