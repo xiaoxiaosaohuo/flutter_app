4ac9e5bb49b162e28ccf6b1e9a12f660a9d2d84e
小小骚货 <jinxin479@126.com>
2019-08-14 01:48:14 +0800

    动画
    动画

--- lib/widgets/animation/chart01.dart
+++ lib/widgets/animation/chart01.dart
@@ -1,17 +1,42 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
+import 'dart:ui' show lerpDouble;
 
 class Chart01 extends StatefulWidget{
   _Chart01State createState()=>_Chart01State();
 }
 
-class _Chart01State extends State<Chart01>{
+class _Chart01State extends State<Chart01> with TickerProviderStateMixin{
   final random = Random();
   int dataSet = 50;
-
+  AnimationController animation;
+  double startHeight;   // Strike one.
+  double currentHeight; // Strike two.
+  double endHeight;     // Strike three. Refactor.
+  @override
+  void initState() {
+    super.initState();
+    animation = AnimationController(duration: Duration(milliseconds: 500),vsync: this)
+                ..addListener((){
+                  setState((){
+                    currentHeight = lerpDouble( // Strike one.
+                      startHeight,
+                      endHeight,
+                      animation.value,
+                    );
+                  });
+                }); 
+    startHeight = 0.0;                // Strike two.
+    currentHeight = 0.0;
+    endHeight = dataSet.toDouble();
+    animation.forward(); 
+  }
   void changeData() {
     setState(() {
+      startHeight = currentHeight;    // Strike three. Refactor.
       dataSet = random.nextInt(100);
+      endHeight = dataSet.toDouble();
+      animation.forward(from: 0.0);
     });
   }
   @override
@@ -24,16 +49,21 @@ class _Chart01State extends State<Chart01>{
           child: Center(
             child: CustomPaint(
               size: Size(200.0, 100.0),
-              painter: BarChartPainter(dataSet.toDouble()),
+              painter: BarChartPainter(currentHeight),
             ),
           ),
       ),
       floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.refresh),
+          child: Icon(Icons.add),
           onPressed: changeData,
         ),
       );
     }
+    @override
+  void dispose() {
+    animation.dispose();
+    super.dispose();
+  }
 }
 
 
