23a2f91e77eb1bbfff2dfcc76e7066819fa8d465
小小骚货 <jinxin479@126.com>
2019-08-15 10:23:18 +0800

    feat
    将动画值抽象到bar 的高度变化

--- lib/widgets/animation/chart01.dart
+++ lib/widgets/animation/chart01.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
 import 'dart:ui' show lerpDouble;
-
+import 'bar.dart';
 class Chart01 extends StatefulWidget{
   _Chart01State createState()=>_Chart01State();
 }
@@ -10,24 +10,18 @@ class _Chart01State extends State<Chart01> with TickerProviderStateMixin{
   final random = Random();
   int dataSet = 50;
   AnimationController animationController;
-  Tween<double> tween;
-  double startHeight;   // Strike one.
-  double currentHeight; // Strike two.
-  double endHeight;     // Strike three. Refactor.
+  BarTween tween;    // Strike three. Refactor.
   @override
   void initState() {
     super.initState();
     animationController = AnimationController(duration: Duration(milliseconds: 500),vsync: this);
-    tween = Tween<double>(begin: 0.0,end:dataSet.toDouble());
+    tween = BarTween(Bar(0.0),Bar(50.0));
     animationController.forward(); 
   }
   void changeData() {
     setState(() {
       dataSet = random.nextInt(100);
-      tween = Tween<double>(
-        begin: tween.evaluate(animationController),
-        end:dataSet.toDouble()
-      );
+      tween = BarTween(tween.evaluate(animationController),Bar(random.nextDouble() * 100.0));
       animationController.forward(from: 0.0);
     });
   }
@@ -59,25 +53,25 @@ class _Chart01State extends State<Chart01> with TickerProviderStateMixin{
 }
 
 
-class BarChartPainter extends CustomPainter{
-  static const barWidth = 10.0;
-  BarChartPainter(this.animation):super(repaint:animation);
-  final Animation<double> animation;
+// class BarChartPainter extends CustomPainter{
+//   static const barWidth = 10.0;
+//   BarChartPainter(this.animation):super(repaint:animation);
+//   final Animation<double> animation;
 
-  @override
-    void paint(Canvas canvas, Size size) {
-      final  barHeight = animation.value;
-      final paint = Paint()
-      ..color = Colors.blue[400]
-      ..style = PaintingStyle.fill;
-      canvas.drawRect(Rect.fromLTWH(
-        (size.width - barWidth) / 2.0,
-        size.height - barHeight,
-        barWidth,
-        barHeight,
-        ),
-        paint);
-    }
-    @override
-    bool shouldRepaint(BarChartPainter old) => false;
-}
+//   @override
+//     void paint(Canvas canvas, Size size) {
+//       final  barHeight = animation.value;
+//       final paint = Paint()
+//       ..color = Colors.blue[400]
+//       ..style = PaintingStyle.fill;
+//       canvas.drawRect(Rect.fromLTWH(
+//         (size.width - barWidth) / 2.0,
+//         size.height - barHeight,
+//         barWidth,
+//         barHeight,
+//         ),
+//         paint);
+//     }
+//     @override
+//     bool shouldRepaint(BarChartPainter old) => false;
+// }
