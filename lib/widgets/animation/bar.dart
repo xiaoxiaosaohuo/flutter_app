30695aeff14e70fe305765f76011ac94185edbbc
jinxin479 <jinxin479@126.com>
2019-08-16 01:04:26 +0800

    feat
    补间动画，合并排序

--- lib/widgets/animation/bar.dart
+++ lib/widgets/animation/bar.dart
@@ -1,38 +1,71 @@
-import 'dart:ui' show lerpDouble;
 import 'dart:math';
+import 'dart:ui' show lerpDouble;
+
 import 'package:flutter/animation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
+
 import 'color_palette.dart';
 
-
 class BarChart {
-  static const int barCount = 5;
+  BarChart(this.bars);
 
-  BarChart(this.bars) {
-    assert(bars.length == barCount);
+  factory BarChart.empty(Size size) {
+    return BarChart(<Bar>[]);
+  }
+
+  factory BarChart.random(Size size, Random random) {
+    const barWidthFraction = 0.75;
+    final ranks = selectRanks(random, ColorPalette.primary.length);
+    final barCount = ranks.length;
+    final barDistance = size.width / (1 + barCount);
+    final barWidth = barDistance * barWidthFraction;
+    final startX = barDistance - barWidth / 2;
+    final bars = List.generate(
+        barCount,
+        (i) => Bar(
+              ranks[i],
+              startX + i * barDistance,
+              barWidth,
+              random.nextDouble() * size.height,
+              ColorPalette.primary[ranks[i]],
+            ));
+    return BarChart(bars);
+  }
+
+  static List<int> selectRanks(Random random, int cap) {
+    final ranks = <int>[];
+    var rank = 0;
+    while (true) {
+      if (random.nextDouble() < 0.2) rank++;
+      if (cap <= rank) break;
+      ranks.add(rank);
+      rank++;
+    }
+    return ranks;
   }
 
   final List<Bar> bars;
 
-  factory BarChart.empty() {
-    return BarChart(List.filled(
-      barCount,
-      Bar(0.0,Colors.transparent)
-    ));
-  }
-
-  factory BarChart.random(Random random){
-    final Color color = ColorPalette.primary.random(random);
-    // 生成bar 列表
-    return BarChart(List.generate(barCount, (i)=>Bar(random.nextDouble() * 100.0,color)));
-  }
-
-  static BarChart lerp (BarChart begin,BarChart end,double t){
-    return BarChart(List.generate(
-      barCount,
-      (i) => Bar.lerp(begin.bars[i], end.bars[i], t),
-    ));
+  static BarChart lerp(BarChart begin, BarChart end, double t) {
+    final bars = <Bar>[];
+    final bMax = begin.bars.length;
+    final eMax = end.bars.length;
+    var b = 0;
+    var e = 0;
+    while (b + e < bMax + eMax) {
+      if (b < bMax && (e == eMax || begin.bars[b] < end.bars[e])) {
+        bars.add(Bar.lerp(begin.bars[b], begin.bars[b].collapsed, t));
+        b++;
+      } else if (e < eMax && (b == bMax || end.bars[e] < begin.bars[b])) {
+        bars.add(Bar.lerp(end.bars[e].collapsed, end.bars[e], t));
+        e++;
+      } else {
+        bars.add(Bar.lerp(begin.bars[b], end.bars[e], t));
+        b++;
+        e++;
+      }
+    }
+    return BarChart(bars);
   }
 }
 
@@ -43,39 +76,41 @@ class BarChartTween extends Tween<BarChart> {
   BarChart lerp(double t) => BarChart.lerp(begin, end, t);
 }
 
-
 class Bar {
-  Bar(this.height,this.color);
+  Bar(this.rank, this.x, this.width, this.height, this.color);
+
+  final int rank;
+  final double x;
+  final double width;
   final double height;
   final Color color;
 
-  static Bar lerp(Bar begin,Bar end,double t){
-    return Bar(
-      lerpDouble(begin.height, end.height, t),
-      Color.lerp(begin.color,end.color,t)
-      );
-  }
-  
-  factory Bar.empty() => Bar(0.0,Colors.transparent);
+  Bar get collapsed => Bar(rank, x, 0.0, 0.0, color);
 
-  factory Bar.random(Random random){
+  bool operator <(Bar other) => rank < other.rank;
+
+  static Bar lerp(Bar begin, Bar end, double t) {
+    assert(begin.rank == end.rank);
     return Bar(
-      random.nextDouble() * 100.0,
-      ColorPalette.primary.random(random),
-      );
+      begin.rank,
+      lerpDouble(begin.x, end.x, t),
+      lerpDouble(begin.width, end.width, t),
+      lerpDouble(begin.height, end.height, t),
+      Color.lerp(begin.color, end.color, t),
+    );
   }
 }
 
+class BarTween extends Tween<Bar> {
+  BarTween(Bar begin, Bar end) : super(begin: begin, end: end) {
+    assert(begin.rank == end.rank);
+  }
 
-// class BarTween extends Tween<Bar> {
-//   BarTween(Bar begin,Bar end): super(begin:begin,end:end);
-//   @override
-//   Bar lerp(double t)=>Bar.lerp(begin,end,t);
-// }
+  @override
+  Bar lerp(double t) => Bar.lerp(begin, end, t);
+}
 
 class BarChartPainter extends CustomPainter {
-  static const barWidthFraction = 0.75;
-
   BarChartPainter(Animation<BarChart> animation)
       : animation = animation,
         super(repaint: animation);
@@ -84,22 +119,19 @@ class BarChartPainter extends CustomPainter {
 
   @override
   void paint(Canvas canvas, Size size) {
-    void drawBar(Bar bar, double x, double width, Paint paint) {
-      paint.color = bar.color;
-      canvas.drawRect(
-        Rect.fromLTWH(x, size.height - bar.height, width, bar.height),
-        paint,
-      );
-    }
-
     final paint = Paint()..style = PaintingStyle.fill;
     final chart = animation.value;
-    final barDistance = size.width / (1 + chart.bars.length);
-    final barWidth = barDistance * barWidthFraction;
-    var x = barDistance - barWidth / 2;
     for (final bar in chart.bars) {
-      drawBar(bar, x, barWidth, paint);
-      x += barDistance;
+      paint.color = bar.color;
+      canvas.drawRect(
+        Rect.fromLTWH(
+          bar.x,
+          size.height - bar.height,
+          bar.width,
+          bar.height,
+        ),
+        paint,
+      );
     }
   }
 
