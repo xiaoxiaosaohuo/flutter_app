efdabc07606ac4e2fff585e6a136a3464ce7df66
小小骚货 <jinxin479@126.com>
2019-08-21 20:09:43 +0800

    feat
    删除文件

--- lib/routers/router_handler.dart
+++ lib/routers/router_handler.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:fluro/fluro.dart';
-import 'package:flutter_app/pages/app.dart';
 import 'package:flutter_app/pages/LoginPage.dart';
 import 'package:flutter_app/pages/animation_page.dart';
 import 'package:flutter_app/widgets/animation/chart01.dart';
+import 'package:flutter_app/pages/main_page.dart';
 // app的首页
 Handler mainHandler = new Handler(
   handlerFunc: (BuildContext context, Map<String, List<String>> params) {
-    return new App();
+    return MainPage ();
   },
 );
 
