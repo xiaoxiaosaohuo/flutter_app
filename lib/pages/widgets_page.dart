f8ba0fe0c5ba2ca478d76f9e11cc073b3066b57e
小小骚货 <jinxin479@126.com>
2019-08-26 22:13:22 +0800

    e
    e

--- lib/pages/widgets_page.dart
+++ lib/pages/widgets_page.dart
@@ -1,81 +1,9 @@
 import 'package:flutter/material.dart';
 import 'dart:math' as Math;
 import 'package:flutter_app/widgets/rate.dart';
-// import '../routers/application.dart';
-class _MyClipper extends CustomClipper<Rect>{
-  @override
-  Rect getClip(Size size) {
-    return new Rect.fromLTRB(10.0, 10.0, size.width - 10.0,  size.height- 10.0);
-  }
-
-  @override
-  bool shouldReclip(CustomClipper<Rect> oldClipper) {
-      return true;
-  }
-
-}
-
-
-
-class _StarCliper extends CustomClipper<Path>{
-
-  final double radius;
-
-  _StarCliper({this.radius});
-
-  /// 角度转弧度公式
-  double degree2Radian(int degree) {
-    return (Math.pi * degree / 180);
-  }
-
-  @override
-  Path getClip(Size size) {
-    double radius = this.radius;
-    Path path = new Path();
-    double radian = degree2Radian(36);// 36为五角星的角度
-    double radius_in = (radius * Math.sin(radian / 2) / Math
-        .cos(radian)); // 中间五边形的半径
-
-    path.moveTo((radius * Math.cos(radian / 2)), 0.0);// 此点为多边形的起点
-    path.lineTo((radius * Math.cos(radian / 2) + radius_in
-        * Math.sin(radian)),
-        (radius - radius * Math.sin(radian / 2)));
-    path.lineTo((radius * Math.cos(radian / 2) * 2),
-        (radius - radius * Math.sin(radian / 2)));
-    path.lineTo((radius * Math.cos(radian / 2) + radius_in
-        * Math.cos(radian / 2)),
-        (radius + radius_in * Math.sin(radian / 2)));
-    path.lineTo(
-        (radius * Math.cos(radian / 2) + radius
-            * Math.sin(radian)), (radius + radius
-        * Math.cos(radian)));
-    path.lineTo((radius * Math.cos(radian / 2)),
-        (radius + radius_in));
-    path.lineTo(
-        (radius * Math.cos(radian / 2) - radius
-            * Math.sin(radian)), (radius + radius
-        * Math.cos(radian)));
-    path.lineTo((radius * Math.cos(radian / 2) - radius_in
-        * Math.cos(radian / 2)),
-        (radius + radius_in * Math.sin(radian / 2)));
-    path.lineTo(0.0, (radius - radius * Math.sin(radian / 2)));
-    path.lineTo((radius * Math.cos(radian / 2) - radius_in
-        * Math.sin(radian)),
-        (radius - radius * Math.sin(radian / 2)));
-
-    path.close();// 使这些点构成封闭的多边形
-
-    return path;
-  }
-
-  @override
-  bool shouldReclip(_StarCliper oldClipper) {
-      return this.radius != oldClipper.radius;
-  }
-
-}
-
-
+import 'package:flutter_app/widgets/list/basic_list.dart';
+import 'package:flutter_app/widgets/list/sticky_list.dart';
+import 'package:flutter_app/widgets/list/customer_list.dart';
 class WidgetPage extends StatefulWidget{
   @override
     State<StatefulWidget> createState() {
@@ -88,66 +16,10 @@ class WidgetPageState extends State<WidgetPage>{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-       appBar: AppBar(
-          title: Text("Widget"),
-        ),
-        body:
-        Container(
-          alignment: Alignment.center,
-          child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-              ClipOval(
-                child:SizedBox(
-                  width: 100.0,
-                  height:100.0,
-                  child:Image.asset("assets/images/1.jpg",fit: BoxFit.fill,),
-                ),
-              ),
-            ClipRRect(
-              borderRadius:BorderRadius.all(
-                  Radius.circular(10.0),
-              ),
-              child: SizedBox(
-                width: 150.0,
-                height:150.0,
-                child:Image.asset("assets/images/1.jpg",fit: BoxFit.fill,)
-
-              ),
-            ),
-             new ClipRect(
-                clipper: new _MyClipper(),
-                child:new SizedBox(
-                  width: 100.0,
-                  height:100.0,
-                  child: Image.asset("assets/images/1.jpg",fit: BoxFit.fill,)
-                ) ,
-              ),
-              StaticRatingBar(),
-              ClipPath(
-                clipper: new _StarCliper(radius: 50.0),
-                child: new SizedBox(
-                  width: 100.0,
-                  height: 100.0,
-                  child: Image.asset("assets/images/1.jpg",fit: BoxFit.fill,),
-                ),
-              ),
-              RatingBar(
-                    size: 30.0,
-                    radiusRatio: 1.4,
-                    onChange: (int value) {
-                      setState(() {
-                        starValue = value;
-                      });
-                    },
-              ),
-          ],
-        )
-        )
-        
-
-        
+      //  appBar: AppBar(
+      //     title: Text("Widget"),
+      //   ),
+        body:CustomSliverList()
       );
   }
 }
---
+++ lib/widgets/list/sticky_list.dart
@@ -0,0 +1,86 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+class StickyList extends StatefulWidget {
+  final String title;
+
+  StickyList({Key key, this.title}) : super(key: key);
+
+  @override
+  _StickyListState createState() => _StickyListState();
+}
+
+class _StickyListState extends State<StickyList>
+    with SingleTickerProviderStateMixin {
+  TabController tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    this.tabController = TabController(length: 2, vsync: this);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            pinned: true,
+            elevation: 0,
+            expandedHeight: 250,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text(this.widget.title),
+              background: Image.asset(
+                'assets/images/italy08.jpg',
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          SliverPersistentHeader(
+            pinned: true,
+            delegate: StickyTabBarDelegate(
+              child: TabBar(
+                labelColor: Colors.black,
+                controller: this.tabController,
+                tabs: <Widget>[
+                  Tab(text: 'Home'),
+                  Tab(text: 'Profile'),
+                ],
+              ),
+            ),
+          ),
+          SliverFillRemaining(
+            child: TabBarView(
+              controller: this.tabController,
+              children: <Widget>[
+                Center(child: Text('Content of Home')),
+                Center(child: Text('Content of Profile')),
+              ],
+            ),
+          ),
+        ],
+    );
+  }
+}
+
+class StickyTabBarDelegate extends SliverPersistentHeaderDelegate {
+  final TabBar child;
+
+  StickyTabBarDelegate({@required this.child});
+
+  @override
+  Widget build(BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return this.child;
+  }
+
+  @override
+  double get maxExtent => this.child.preferredSize.height;
+
+  @override
+  double get minExtent => this.child.preferredSize.height;
+
+  @override
+  bool shouldRebuild(SliverPersistentHeaderDelegate oldDelegate) {
+    return true;
+  }
+}
