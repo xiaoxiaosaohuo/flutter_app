eca2fc573cb33e870716499a1812cb3f6f3564a2
nishang.jx <nishang.jx@autonavi.com>
2019-08-28 19:21:38 +0800

    feat
    卡片动画

---
+++ lib/widgets/animation/slide_card/index.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/widgets/animation/slide_card/bottom_sheet.dart';
+import 'package:flutter_app/widgets/animation/slide_card/sliding_card.dart';
+import 'package:flutter_app/widgets/animation/slide_card/tabs.dart';
+
+
+class SlideCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: <Widget>[
+          SafeArea(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                SizedBox(height: 8),
+                Header(),
+                SizedBox(height: 40),
+                Tabs(),
+                SizedBox(height: 8),
+                SlidingCardsView(),
+              ],
+            ),
+          ),
+          ExhibitionBottomSheet(),
+        ],
+      ),
+    );
+  }
+}
+
+class Header extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 32),
+      child: Text(
+        'Shenzhen',
+        style: TextStyle(
+          fontSize: 22,
+          fontWeight: FontWeight.w600,
+        ),
+      ),
+    );
+  }
+}
